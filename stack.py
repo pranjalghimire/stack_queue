77f5b53be432959defb53a04578bb83e63bc0007
Pranjal Ghimire <ghimirepranjal@protonmail.com>
2020-05-30 03:38:21 -0500

    stack completed

---
+++ stack.py
@@ -0,0 +1,21 @@
+class Stack():
+  def __init__(self,list):
+    if not list:
+        self.list=[]
+    else:
+        self.list=list
+
+  def push(self,value):
+      self.list.append(value)
+
+  def pop(self):
+      return self.list.pop()
+
+  def printstack(self):
+       print(self.list)
+
+a=Stack([1,2,3])
+a.push(1)
+a.push(2)
+a.pop()
+a.printstack()
